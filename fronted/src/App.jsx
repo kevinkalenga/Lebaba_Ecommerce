5491ab14c0c55bf5e418f800ff2218198799c300
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-03 12:38:53 +0100

    Setup react-router-dom

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,13 +1,16 @@
-
+import { Outlet } from 'react-router'
 import './App.css'
 
 function App() {
 
 
   return (
-    <h1 className="bg-red-600 my-0 text-center p-5 text-white font-bold">
-      Hello World
-    </h1>
+    <>
+      <h1 className="bg-red-600 my-0 text-center p-5 text-white font-bold">
+        Hello World
+      </h1>
+      <Outlet />
+    </>
   )
 }
 
