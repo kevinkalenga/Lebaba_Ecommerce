1e44b0f8f2f69c501f5ab22622eb1057ba06620d
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-03 16:10:24 +0100

    Home Categories

---
+++ fronted/src/pages/home/Categories.jsx
@@ -0,0 +1,48 @@
+import category1 from "../../assets/category-1.jpg"
+import category2 from "../../assets/category-2.jpg"
+import category3 from "../../assets/category-3.jpg"
+import category4 from "../../assets/category-4.jpg"
+import { Link } from 'react-router-dom'
+
+const Categories = () => {
+
+    const categories = [
+        {
+            name: 'Accessoories',
+            path: 'accessories',
+            image: category1
+        },
+        {
+            name: 'Dress Collection',
+            path: 'dress',
+            image: category2
+        },
+        {
+            name: 'Jewelery',
+            path: 'jewelery',
+            image: category3
+        },
+        {
+            name: 'Cosmetics',
+            path: 'cosmetics',
+            image: category4
+        },
+    ]
+
+    return (
+        <>
+            <div className="product__grid">
+                {
+                    categories.map((category) => (
+                        <Link to={`/categories/${category.path}`} className='categories__card'>
+                            <img src={category.image} alt={category.name} />
+                            <h3>{category.name}</h3>
+                        </Link>
+                    ))
+                }
+            </div>
+        </>
+    )
+}
+
+export default Categories
