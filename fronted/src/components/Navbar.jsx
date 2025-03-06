70477ed428895682c4c40702878ff017db518938
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-06 13:48:18 +0100

    Intall Redux Toolkit and Create Cart Slice

--- fronted/src/components/Navbar.jsx
+++ fronted/src/components/Navbar.jsx
@@ -1,5 +1,10 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux'
 const Navbar = () => {
+
+    const products = useSelector((state) => state.cart.products);
+    console.log(products)
+
     return (
         <header className="fixed-nav-bar w-nav">
             <nav className='max-w-screen-2xl mx-auto px-4 justify-between items-center'>
@@ -34,12 +39,12 @@ const Navbar = () => {
                         <button className='hover:text-primary'>
                             <i className="ri-shopping-cart-line"></i>
                             <sup className='text-sm inline-block px-1.5
-                             text-white rounded-full bg-primary text-center'>0</sup>
+                             text-white rounded-full bg-primary text-center'>{products.length}</sup>
                         </button>
                     </span>
                     <span>
                         <Link to="login">
-                            <i class="ri-user-line"></i>
+                            <i className="ri-user-line"></i>
                         </Link>
                     </span>
                 </div>
--- fronted/src/main.jsx
+++ fronted/src/main.jsx
@@ -4,9 +4,11 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/router.jsx'
 import 'remixicon/fonts/remixicon.css'
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </Provider>,
 )
