f00b3e23291b7a1b23325917aefec1b381ab42bc
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-07 12:00:53 +0100

    Install Mongodb and Mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
+const mongoose = require('mongoose');
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+
+
+main().then(() => console.log("Mongodb is successfully connected.")).catch(err => console.log(err))
+
+async function main() {
+    await mongoose.connect('mongodb+srv://kalenga10:kalenga10@lebaba.i8bco.mongodb.net/lebaba_project?retryWrites=true&w=majority&appName=lebaba');
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+}
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
