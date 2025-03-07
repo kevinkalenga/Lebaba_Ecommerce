1b5d042e511855f59580b295b1277527c9dbed0b
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-07 16:01:34 +0100

    Create User Route

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,13 @@ app.use(cors({
     credentials: true
 }))
 
+// all routes
+
+const authRoutes = require('./src/users/user.route');
+
+app.use('/api/auth', authRoutes)
+
+
 main().then(() => console.log("Mongodb is successfully connected.")).catch(err => console.log(err))
 
 async function main() {
