2fa2a8540c66253efb6b4e8042a9a6f032167366
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-07 10:46:07 +0100

    Register Page

--- fronted/src/routers/router.jsx
+++ fronted/src/routers/router.jsx
@@ -6,6 +6,7 @@ import Search from "../pages/search/Search";
 import ShopPage from '../pages/shop/ShopPage';
 import SingleProduct from '../pages/shop/productDetails/SingleProduct';
 import Login from '../components/Login';
+import Register from '../components/Register';
 const router = createBrowserRouter([
     {
         path: "/",
@@ -22,7 +23,11 @@ const router = createBrowserRouter([
     {
         path: "/login",
         element: <Login />
-    }
+    },
+    {
+        path: "/register",
+        element: <Register />
+    },
 ])
 
 export default router
