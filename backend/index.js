37c7a05fe2eec11660b323f0df1f6bbcbe2d18eb
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-07 12:42:54 +0100

    Install dotenv, cors, cookie-parser and body-parser packages

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose');
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const app = express()
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 5000
 
-
+// middleware setup
+app.use(express.json({ limit: "25mb" }));
+app.use((express.urlencoded({ limit: "25mb" })));
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    credentials: true
+}))
 
 main().then(() => console.log("Mongodb is successfully connected.")).catch(err => console.log(err))
 
 async function main() {
-    await mongoose.connect('mongodb+srv://kalenga10:kalenga10@lebaba.i8bco.mongodb.net/lebaba_project?retryWrites=true&w=majority&appName=lebaba');
+    await mongoose.connect(process.env.DB_URL);
 
     app.get('/', (req, res) => {
         res.send('Hello World!')
@@ -16,5 +29,5 @@ async function main() {
 }
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}!!!`)
 })
