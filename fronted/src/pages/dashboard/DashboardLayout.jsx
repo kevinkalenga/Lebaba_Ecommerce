6538ee0dc3453e4b72bdf8fc8ba473c56c6c9075
kevin kalenga <kevinkalenga10@gmail.com>
2025-07-08 10:04:36 +0200

    Dashboard Layout

--- fronted/src/pages/dashboard/DashboardLayout.jsx
+++ fronted/src/pages/dashboard/DashboardLayout.jsx
@@ -1,10 +1,34 @@
 
-import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Navigate, Outlet } from 'react-router-dom'
 
 const DashboardLayout = () => {
-  return (
-    <div>
-        <Outlet />
+    const {user} = useSelector((state) => state.auth)
+    if(!user) {
+       return <Navigate to="/login" replace/> 
+    }
+  
+    const renderDashboard = () => {
+        switch(user?.role) {
+            case 'admin':
+                return <div>Admin Dashboard</div>
+            case 'user':
+                return <div>User Dashboard</div> 
+            default:
+                return <Navigate to="/login" replace />
+
+        }
+    }
+   return (
+    <div className='container mx-auto flex flex-col 
+        md:flex-row gap-4 items-start justify-start'>
+        <header className='lg:w-1/5 sm:w-2/5 w-full border'>
+           {renderDashboard()}
+        </header>
+        <main className='p-8 bg-white w-full border mt-5'>
+           <Outlet />
+        </main>
+        
     </div>
   )
 }
