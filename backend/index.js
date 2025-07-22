ee0b0956d0b0553580b06daf0429261f46856879
kevin kalenga <kevinkalenga10@gmail.com>
2025-07-22 18:58:31 +0200

    Upload image using cloudinary and post a new product

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,9 @@ app.use(cors({
     credentials: true
 }))
 
+// image upload 
+const uploadImage = require("./src/utils/uploadImage")
+
 // all routes
 
 const authRoutes = require('./src/users/user.route');
@@ -48,6 +51,14 @@ async function main() {
         res.send('Hello World!')
     })
 }
+
+
+app.post("/uploadImage", (req, res) => {
+  uploadImage(req.body.image)
+    .then((url) => res.send(url))
+    .catch((err) => res.status(500).send(err));
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!!!`)
--- fronted/src/pages/dashboard/admin/addProduct/UploadImage.jsx
+++ fronted/src/pages/dashboard/admin/addProduct/UploadImage.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import axios from 'axios'
+import { getBaseUrl } from '../../../../utils/baseURL';
+
 
 
 
@@ -7,9 +10,59 @@ const UploadImage = ({ name, setImage }) => {
     const [loading, setLoading] = useState(false);
     const [url, setUrl] = useState("");
 
+     // base64 functionality
+
+    const convertBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(file);
+
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+
+            fileReader.onerror = (error) => {
+                reject(error);
+            };
+        });
+    };
+
+    
+     // request to upload a file
+    const uploadSingleImage = (base64) => {
+        setLoading(true);
+        axios
+            .post(`${getBaseUrl()}/uploadImage`, { image: base64 })
+            .then((res) => {
+                const imageUrl = res.data;
+                setUrl(imageUrl);
+                // console.log(imageUrl);
+                alert("Image uploaded successfully");
+                setImage(imageUrl); 
+            })
+            .then(() => setLoading(false))
+            .catch((error) => {
+                console.error(error);
+                setLoading(false);
+            });
+    };
+    
+    
     
     const uploadImage = async (event) => {
-     
+          const files = event.target.files;
+
+        if (files.length === 1) {
+            const base64 = await convertBase64(files[0]);
+            uploadSingleImage(base64);
+            return;
+        }
+
+        const base64s = [];
+        for (let i = 0; i < files.length; i++) {
+            const base = await convertBase64(files[i]);
+            base64s.push(base);
+        }
     }
 
 
